71486235df63ea1c4c1fe6b30998c97b9c6775fa
bahalahmad <ahmadbahal786@outlook.com>
2024-11-21 15:56:06 +0530

    main method added

---
+++ src/main/java/com/kb/population/visualization/PopulationVisualizationApplication.java
@@ -0,0 +1,13 @@
+package com.kb.population.visualization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopulationVisualizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopulationVisualizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kb/population/visualization/PopulationVisualizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kb.population.visualization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopulationVisualizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
